7c176deb8af1ee223b2e4d066e4082f8a564a6b5
18838078480@163.com <antiphon>
2017-08-30 10:10:01 +0800

    Initial commit

---
+++ app/src/main/java/com/willpower/touch/SimpleActivity.java
@@ -0,0 +1,13 @@
+package com.willpower.touch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple);
+    }
+}
